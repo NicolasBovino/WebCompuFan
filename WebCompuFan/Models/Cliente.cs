22d2e476c18a20c6fb864c74a574e824b1d543ba
nicol <nicol@DESKTOP-U7CSUA7>
2023-11-06 19:49:20 -0300

    Agregar archivos de proyecto.

---
+++ WebCompuFan/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+namespace MVCBasico.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
